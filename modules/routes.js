cacd1f0dd61b8ff877e3346131888754bdaee527
Mr. Smith <csmith@ytech.edu>
2024-12-20 12:56:00 -0500

    back to lunch

--- modules/routes.js
+++ modules/routes.js
@@ -10,30 +10,51 @@ const loginGET = (req, res) => {
 };
 
 const loginPOST = (req, res) => {
-    const loginPOST = (req, res) => {
-        const { username, password } = req.body;
-
-        if (!username || !password) {
-            return res.status(400).send('Username and password are required');
-        }
-
-        const hash = crypto.createHash('sha256').update(password).digest('hex');
-
-        db.getUserByUsername(username, (err, user) => {
+    if (req.body.user && req.body.pass) {
+        db.get("SELECT * FROM users WHERE username=?;", req.body.user, (err, row) => {
             if (err) {
-                return res.status(500).send('Internal server error');
+                console.error(err);
+                res.send("There was an error:\n" + err);
+            } else if (!row) {
+                // Create a new salt for this user
+                const salt = crypto.randomBytes(16).toString("hex");
+
+                // Use this salt to "hash" the password
+                crypto.pbkdf2(req.body.pass, salt, 1000, 64, "sha512", (err, derivedKey) => {
+                    if (err) {
+                        res.send("Error hashing password: " + err);
+                    } else {
+                        const hashedPassword = derivedKey.toString("hex");
+                        db.run("INSERT INTO users (username, password, salt) VALUES (?, ?, ?);", [req.body.user, hashedPassword, salt], (err) => {
+                            if (err) {
+                                res.send("Database error:\n" + err);
+                            } else {
+                                res.send("Created new user");
+                            }
+                        });
+                    }
+                });
+
+            } else {
+                // Compare stored password with provided password
+                crypto.pbkdf2(req.body.pass, row.salt, 1000, 64, "sha512", (err, derivedKey) => {
+                    if (err) {
+                        res.send("Error hashing password: " + err);
+                    } else {
+                        const hashedPassword = derivedKey.toString("hex");
+                        if (row.password === hashedPassword) {
+                            req.session.user = row.username;
+                            res.redirect("/home");
+                        } else {
+                            res.send("Incorrect password");
+                        }
+                    }
+                });
             }
-
-            if (!user || user.password !== hash) {
-                return res.status(401).send('Invalid username or password');
-            }
-
-            // Assuming you have a function to create a session or token
-            const token = createSession(user);
-
-            res.status(200).send({ message: 'Login successful', token });
         });
-    };
+    } else {
+        res.send("You need a username and password");
+    }
 };
 
 const logoutGET = (req, res) => {
